227772a95fd016bbebaaa09e4b47551018f25e9d
mariomeneses <mario.meneses.a@gmail.com>
2014-07-10 13:39:27 -0400

    prueba

--- prueba2/post.php
+++ prueba2/post.php
@@ -12,10 +12,10 @@
 	}
 
 	function val_email($email1){
-		if (preg_match("/^[\w-\.]{3,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}+$/", $email1)) {
-			return false;
-		}else{
+		if (preg_match("/^[A-Za-z0-9-_.+%]+@[A-Za-z0-9-.]+\.[A-Za-z]{2,4}$/", $email1)) {
 			return true;
+		}else{
+			return false;
 		}
 	}
 
